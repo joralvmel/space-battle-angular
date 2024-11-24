06e258d52b1add85490bc76c5d8b4736c3e58860
joralvmel <joralvmel@gmail.com>
2024-11-24 15:50:41 +0100

    add navigation and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {PreferencesComponent} from './preferences/preferences.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {PlayComponent} from './play/play.component';
+import {RecordsComponent} from './records/records.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'preferences', component: PreferencesComponent },
+  { path: 'play', component: PlayComponent },
+  { path: 'records', component: RecordsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
